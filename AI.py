c0b9fb8fbd937d19b964289a4fb47a0ce9598a2a
unknown <cferrara212@gmail.com>
2021-10-08 13:36:27 -0600

    fixed syntax in Ai file

--- AI.py
+++ AI.py
@@ -1,6 +1,6 @@
 from player import Player
 class Ai(Player):
-    def __init__(Player):
+    def __init__(self):
         self.name = "Artificial Human"
         self.gesture = None
         super().__init__()
