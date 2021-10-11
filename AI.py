d9dcbccdffcac2f0ea40746680bc474735801b7d
unknown <yoshuah809@gmail.com>
2021-10-11 15:18:50 -0500

    Simplifying Set Gesture Code to comply with Solid Standard

--- AI.py
+++ AI.py
@@ -9,14 +9,5 @@ class AI(Player):
         self.gesture = None
 
     def set_gesture(self):
-        gesture_selection = random.randint(0,4)
-        if gesture_selection == 0:
-            self.gesture = self.gesture_list[0]
-        if gesture_selection == 1:
-            self.gesture = self.gesture_list[1]
-        if gesture_selection == 2:
-            self.gesture = self.gesture_list[2]
-        if gesture_selection == 3:
-            self.gesture = self.gesture_list[3]
-        if gesture_selection == 4:
-            self.gesture = self.gesture_list[4]
+        self.gesture = self.gesture_list[random.randint(0,4)]
+        
