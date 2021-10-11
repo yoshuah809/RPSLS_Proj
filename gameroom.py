7ba222d260083831e0d083777b423fe9289d9aa3
unknown <yoshuah809@gmail.com>
2021-10-11 11:03:30 -0500

    Added Method Display Options so that user can select the gesture option

--- gameroom.py
+++ gameroom.py
@@ -6,10 +6,13 @@ from AI import AI
 
 class Gameroom():
     def __init__(self):
-        # self.player = Human()
-        # self.computer = AI()
         self.display_welcome()
         self.display_rules()
+        self.select_game_mode()
+         # Here the Player One Select the gesture
+        self.player_one.gesture = self.player_one.gesture_list[self.display_options()] 
+        # From here the machine takes the turn 
+        #print(self.player_one.gesture) 
 
     def display_welcome(self):
         print('Welcome to Rock, Paper, Scissors, Lizard, Spock, or RPSLS for short!')
@@ -34,17 +37,31 @@ class Gameroom():
         print('spock vaporizes rock')
         input('\n'+'\n'+'Once you have read and understand the rules, please hit the enter key')
 
-    def select_game_mode():
-        pass
+    def select_game_mode(self):
+        self.selected_option = int(input("Please select 1. for Player Vs Player or 2. For Player Vs Machine (Single Mode: ): ")) 
+        if self.selected_option == 1:  #user has selected 2 Players
+           self.player_one = Human()
+           self.player_two = Human()
+           print("You have selected Multiplayer Mode, Choose your gesture below...")
+        
+        elif self.selected_option == 2:    #user has selected Single mode
+            self.player_one = Human()
+            self.player_two = AI()
+            print("You have selected Single player Mode")
+        else:
+            print("Plese select a Valid option, Press Enter to continue") 
+            input('press the enter key to continue.') 
+   
 
-    def pick_gesture():
-        pass
+    def display_options(self):
+        
+        for gesture in range(len(self.player_one.gesture_list)):
+            print(f'Please select {gesture} for {self.player_one.gesture_list[gesture]}')
 
-    def display_options():
-        pass
+        return_gesture = int(input())    
+        return return_gesture  
 
     def compare_gesture():
         pass
-
-    def select_game_mode():
+    def pick_gesture():
         pass
